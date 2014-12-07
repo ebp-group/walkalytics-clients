f9e5c6a9ac598dd2f3c32ca6facc58fa96db61a3
Stephan Heuel <mail@ping13.net>
2014-12-07 16:35:24 +0100

    parallelization + download of raw data

--- Python/isochrone.py
+++ Python/isochrone.py
@@ -10,15 +10,26 @@ import re
 import math
 import netrc 
 import sys
+import time
+
+from multiprocessing import Pool, freeze_support
+from multiprocessing.dummy import Pool as ThreadPool 
+
 
 # Configuration
 
 # - read x and y coordinates from command line arguments, or just use default coords.
 try:
-    x,y = sys.argv[1:3]
+    sources  = []
+    l = sys.argv[1:]
+    for el in l:
+        sources.append(tuple([int(x) for x in el.split(",")]))
+        assert(len(sources[-1])==2) 
+except AssertionError:
+    print "Error: Arguments should be of the form : x1,y1 (x2,y2) (x3,y3) ..."
+    sys.exit(-1)
 except ValueError:
-    x,y = 828895, 5932832
-print("Source location is ({},{}).".format(x,y))
+    sources  = [ (828895, 5932832) ]
 
 # - set the api_key by reading a netrc file with API key. 
 try:
@@ -33,15 +44,6 @@ _, _, api_key = secrets.authenticators("api.walkalytics.com")
 #   api_key with your key from https://dev.walkalytics.com/developer
 
 
-# - set API parameters and set of POIs
-params = {
-    'x' : x,
-    'y' : y,
-    "only_pois": 'false', # or "true"
-    "outputsize": 512,    # max is currently 720
-    # more parameters are possible, see API documentation
-    'key': api_key 
-}
 
 # - POIs may also be an empty dict
 pois = { "type": "FeatureCollection",
@@ -61,67 +63,113 @@ pois = { "type": "FeatureCollection",
 # - define API call URL
 url = 'https://api.walkalytics.com/v1/isochrone'
 
-# Now make the actual API call as a POST request
-try:
-    req = urllib2.Request("{0}?{1}".format(url,urllib.urlencode(params)), 
-                          json.dumps(pois),
-                          {'Content-Type': 'application/json'})
-    response = urllib2.urlopen(req)
-    result = json.loads(response.read())
-except urllib2.HTTPError, e:
-    if e.code == 204:
-        pass
-    else:
-        print("Error with request: {0}".format(e))
-        sys.exit(-1)
-
-# Check status
-if result.get("status") != "success":
-    print("API call did not succeed. Details: {}".format(result.get("msg")))
-    sys.exit(-1)
-        
-
-# Print out service area in pixel and walking time in minutes for each POI
-service_area_km2 = (result['service_area']) / 1000000.0
-print("The service area for the source location is {:.2f} square km.".format(service_area_km2))
-if result['pois'].has_key('features'):
-    for poi in result['pois']['features']:
-        id = poi['properties']['id']
-        t  = poi['properties']['time'] 
-        if t == -1:
-            print("- POI '{0}' is not covered by the service area of the source point.".format(id))
-        else:
-            print("- POI '{0}' has a walking time of {1} minutes to the source point.".format(id,t/60))
-else:
-    print("No POIs found in the result.")
-
-# Now save the image as a PNG file with an accompanying world file
-if not result.has_key('img'):
-    print("No image was requested.")
-else:
-    basename = "isochrone_{0}_{1}".format(params['x'], params['y'])
-    print("Save image {0}.png with an accompanying world file.".format(basename))
-
+def parse_base64(s):
     # - do a regexp match for extension, code and actual data (this is slightly
     #   more general than necessary)
     pattern = re.compile("^data:.+\/(.+);(.+),(.*)$")
-    match = pattern.search(result['img'])
+    match = pattern.search(s)
     # - extract extension, coding and actual data
     extension = match.group(1)
     code = match.group(2) 
     data = match.group(3)
-    assert(code == "base64") # make sure we have base64
-    assert(extension == "png") # make sure we have a PNG file
+    return (extension, code, data)
 
-    # - write image to a file
-    with open("{0}.{1}".format(basename,extension),"wb") as f:
-        f.write(data.decode(code))
-        
-    # Now save the world file . The parameters for the world file are calculated based on
-    # the lower-left corner, the number of rows and the cellsize.
-    with open("{0}.pgw".format(basename),"wb") as f:
-        f.write("{}\n0.0\n0.0\n".format(result["cellsize"]))
-        f.write("{}\n{}\n{}\n".format(-result["cellsize"],
-                                      result["xllcorner"], 
-                                      result["yllcorner"]+(result["cellsize"]*result["nrows"])))
+def call_walkalytics(source,basename="isochrone"):
+    x,y =  source
+
+    print("Source location is ({},{}).".format(x,y))
+
+    # - set API parameters and set of POIs
+    params = {
+        'x' : x,
+        'y' : y,
+        "only_pois": 'false', # or "true"
+        "outputsize": 512,    # max is currently 720
+        # more parameters are possible, see API documentation
+        'key': api_key,
+        'raw_data': 'true'
+    }
+
+    # Now make the actual API call as a POST request
+    try:
+        req = urllib2.Request("{0}?{1}".format(url,urllib.urlencode(params)), 
+                              json.dumps(pois),
+                              {'Content-Type': 'application/json'})
+        response = urllib2.urlopen(req)
+        result = json.loads(response.read())
+    except urllib2.HTTPError, e:
+        if e.code == 204:
+            pass
+        else:
+            print("Error with request: {0}".format(e))
+            return False
+
+    # Check status
+    if result.get("status") != "success":
+        print("API call did not succeed. Details: {}".format(result.get("msg")))
+        return False
+            
+    # Print out service area in pixel and walking time in minutes for each POI
+    service_area_km2 = (result['service_area']) / 1000000.0
+    print("The service area for the source location is {:.2f} square km.".format(service_area_km2))
+    if result['pois'].has_key('features'):
+        for poi in result['pois']['features']:
+            id = poi['properties']['id']
+            t  = poi['properties']['time'] 
+            if t == -1:
+                print("- POI '{0}' is not covered by the service area of the source point.".format(id))
+            else:
+                print("- POI '{0}' has a walking time of {1} minutes to the source point.".format(id,t/60))
+    else:
+        print("No POIs found in the result.")
+
+    # Now save the image as a PNG file with an accompanying world file
+    if result.has_key('img'):
+        filename = "{}_{}_{}".format(basename,params['x'], params['y'])
+        print("Save image {0}.png with an accompanying world file.".format(filename))
+
+        (extension, code, data) = parse_base64(result['img'])
+        assert(code == "base64") # make sure we have base64
+        assert(extension == "png") # make sure we have a PNG file
+
+        # - write image to a file
+        with open("{0}.{1}".format(filename,extension),"wb") as f:
+            f.write(data.decode(code))
+            
+        # Now save the world file . The parameters for the world file are calculated based on
+        # the lower-left corner, the number of rows and the cellsize.
+        with open("{0}.pgw".format(filename),"wb") as f:
+            f.write("{}\n0.0\n0.0\n".format(result["cellsize"]))
+            f.write("{}\n{}\n{}\n".format(-result["cellsize"],
+                                          result["xllcorner"], 
+                                          result["yllcorner"]+(result["cellsize"]*result["nrows"])))
+    elif result.has_key("raw_data"):
+        filename = "{}_{}_{}".format(basename,params['x'], params['y'])
+        print("Save image {0}.asc.gz.".format(filename))
+        (extension, code, data) = parse_base64(result['raw_data'])
+        assert(code == "base64") # make sure we have base64
+        assert(extension == "gzip") # make sure we have a .gz file
+        # - write image to a file
+        with open("{0}.asc.gz".format(filename),"wb") as f:
+            f.write(data.decode(code))
+    else:
+        print("No image was requested.")
+
+    return True
+            
+if __name__ == '__main__':
+    t0 = time.clock()
+    freeze_support() 
+    pool = ThreadPool(4)
+    pool.map(call_walkalytics, sources)
+    pool.close()
+    pool.join()
+
+    t1 = time.clock()
+
+    print("Effective seconds per call: {:.4f}s".format((t1-t0)/len(sources)))
+
+    # isochrone.py 828895,5932832 828895,5932833 828895,5932834 828895,5932835 828895,5932836 828895,5932837 828895,5932838 828895,5932839 828895,5932830 828895,5932831
+    # for source in sources:
+    #     call_walkalytics(source)
     
